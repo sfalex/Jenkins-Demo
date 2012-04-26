21e7d4b05ec6ef884a75dbb2d1cff1d69e664bb1
sfalex <alexmyerp@gmail.com>
2012-04-26 12:41:45 -0700

    Update test/SmokeTest.java

--- test/SmokeTest.java
+++ test/SmokeTest.java
@@ -8,7 +8,7 @@ public class SmokeTest extends TestingBotTestCase {
             "hub.testingbot.com",
             4444,
             "firefox",
-            "http://www.google.com/");
+            "http://www.myerp.com/");
 
     this.selenium = selenium;
     selenium.start("version=10;platform=WINDOWS;screenrecorder=false");
@@ -17,9 +17,16 @@ public class SmokeTest extends TestingBotTestCase {
     System.out.println("TestingBotSessionID=" + this.selenium.getEval("selenium.sessionId"));
   }
 
-  public void testGoogle() throws Exception {
-    this.selenium.open("/");
-    assertEquals("Google", this.selenium.getTitle());
+  public void testLogin() throws Exception {
+    this.selenium.open("/pp.myerp.com/index.jsp?locale=en_US");
+    assertEquals("Sign In | myERP.com", this.selenium.getTitle());
+    this.selenium.type("//*[@id=\"userUsername\"]", "alex.myerp@gmail.com");
+    this.selenium.fireEvent("//*[@id=\"userUsername\"]", "blur");
+    this.selenium.type("//*[@id=\"userPassword\"]", "311088");
+  	this.selenium.fireEvent("//*[@id=\"userPassword\"]", "blur");
+    this.selenium.waitForPageToLoad("30000");
+  	this.selenium.click("css=input[type=\"image\"]");
+		this.selenium.waitForPageToLoad("30000");
   }
 
   public void tearDown() throws Exception {
